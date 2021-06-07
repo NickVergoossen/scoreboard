32df8380e9e468d7203a1867b46651ed08c69880
VeNi080604 <veni080604@leerling.mosa-RT.be>
2021-06-07 12:54:02 +0200

    scorebord v3

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import datetime
+import sys
+import time
 import pygame
 
-Maaseik = 0
-Roeselare = 0
 WIDTH = 1200
 HEIGHT = 600
+
+score_home = 0
+score_away = 0
+sets_home = 0
+sets_away = 0
+set = 5
+
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-font = pygame.font.SysFont("comicsansms", 90)
+font = pygame.font.SysFont("Arial", 90)
 running = True
 while running:
     for event in pygame.event.get():
@@ -14,17 +22,29 @@ while running:
             running = False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                Maaseik = Maaseik + 1
+                score_home = score_home + 1
 
             if event.key == pygame.K_RIGHT:
-                Roeselare = Roeselare + 1
+                score_away = score_away + 1
             if event.key == pygame.K_r:
-                Maaseik = 0
-                Roeselare = 0
+                score_home = 0
+                score_away = 0
+
+    if score_home >= set and score_home >= score_away + 2:
+        score_home = 0
+        score_away = 0
+        sets_home = sets_home + 1
+
+    if score_away >= set and score_away >= score_home + 2:
+        score_home = 0
+        score_away = 0
+        sets_away = sets_away + 1
+
     screen.fill("white")
 
-    label = f"""De score tijdens de match is {Maaseik} - {Roeselare}"""
-    text = font.render(f"""Maaseik {Maaseik} - {Roeselare}  Roeselare""", True, "black")
+    label = f"""De score tijdens de match is {score_home} - {score_away}"""
+    text = font.render(f"""Maaseik {score_home} - {score_away}  Roeselare""", True, "black")
+    sets = font.render(f"""{sets_home} - {sets_away}""", True, "black")
     tekstbreedte = text.get_width()
     teksthoogte = text.get_height()
     team_A_logo = pygame.image.load("maaseik2tr.png").convert_alpha()
@@ -33,6 +53,8 @@ while running:
     team_B_logo = pygame.transform.smoothscale(team_B_logo, (200, 200))
 
     screen.blit(text, (WIDTH / 2 - tekstbreedte / 2, HEIGHT / 2 - teksthoogte / 2))
-    screen.blit(team_A_logo, (200, 0))
+    screen.blit(team_A_logo, (230, 0))
     screen.blit(team_B_logo, (800, 0))
+    screen.blit(sets, (485, 400))
+
     pygame.display.flip()
