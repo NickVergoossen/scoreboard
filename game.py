180c4335fd1b9cba449d8949d963002e156dac52
VeNi080604 <veni080604@leerling.mosa-rt.be>
2021-09-14 09:43:41 +0200

    scorebord

---
+++ game.py
@@ -0,0 +1,50 @@
+
+
+class Game:
+    """Deze class houdt de stand bij, en voorziet een functie
+    om de nieuwe stand te berekenen.
+    """
+    def __init__(self):
+        """Zet de stand op de beginstand bij aanvang van de wedstrijd"""
+        # maar corrigeer de initiele waardes:
+        self.points_home = 0
+        self.points_away = 0
+        self.sets_home = 0
+        self.sets_away = 0
+        self.set_nr = 1
+
+
+
+    def get_max_points(self):
+        """Geef het maximaal aantal punten voor deze set terug."""
+
+        # de set_nr.
+        if self.set_nr == 5:
+            return 15
+        else:
+            return 25
+
+    def score(self, team):
+        """Bereken de nieuwe stand als team `team` gescoord heeft.
+        team: kan "A" of "H" zijn, voor "away" en "home".
+        """
+        # Gebruik `get_max_points` om te weten bij hoeveel punten je
+        # in de set de set gewonnen hebt.
+        if team == 'H':
+            self.points_home += 1
+
+        if team == 'A':
+            self.points_away += 1
+
+        if self.points_home >= self.get_max_points():
+            if self.points_away < self.points_home - 1:
+                self.points_home = 0
+                self.points_away = 0
+                self.sets_home += 1
+                self.set_nr += 1
+        if self.points_away >= self.get_max_points():
+            if self.points_home < self.points_away - 1:
+                self.points_home = 0
+                self.points_away = 0
+                self.sets_away += 1
+                self.set_nr += 1
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import datetime
 import sys
 import time
 import pygame
+from game import Game
 
 WIDTH = 1200
 HEIGHT = 600
@@ -11,6 +12,7 @@ score_away = 0
 sets_home = 0
 sets_away = 0
 set = 5
+game = Game()
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -23,9 +25,10 @@ while running:
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
                 score_home = score_home + 1
+                game.score("H")
 
             if event.key == pygame.K_RIGHT:
-                score_away = score_away + 1
+                game.score("A")
             if event.key == pygame.K_r:
                 score_home = 0
                 score_away = 0
@@ -42,14 +45,14 @@ while running:
 
     screen.fill("white")
 
-    label = f"""De score tijdens de match is {score_home} - {score_away}"""
-    text = font.render(f"""Maaseik {score_home} - {score_away}  Roeselare""", True, "black")
-    sets = font.render(f"""{sets_home} - {sets_away}""", True, "black")
+    label = f"""De score tijdens de match is {game.points_home} - {game.points_away}"""
+    text = font.render(f"""Maaseik {game.points_home} - {game.points_away}  Roeselare""", True, "black")
+    sets = font.render(f"""{game.sets_home} - {game.sets_away}""", True, "black")
     tekstbreedte = text.get_width()
     teksthoogte = text.get_height()
-    team_A_logo = pygame.image.load("maaseik2tr.png").convert_alpha()
+    team_A_logo = pygame.image.load("maaseik.jpg").convert_alpha()
     team_A_logo = pygame.transform.smoothscale(team_A_logo, (200, 200))
-    team_B_logo = pygame.image.load("roeselaretr.png").convert_alpha()
+    team_B_logo = pygame.image.load("roeselare.png").convert_alpha()
     team_B_logo = pygame.transform.smoothscale(team_B_logo, (200, 200))
 
     screen.blit(text, (WIDTH / 2 - tekstbreedte / 2, HEIGHT / 2 - teksthoogte / 2))
